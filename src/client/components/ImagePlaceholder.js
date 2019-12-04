d2312b21715ef5a7e6f7ef604c40088775047bca
Dennis <d.loska@hotmail.de>
2019-12-04 10:37:06 +0100

    feat: Placeholders for StyleCards

--- src/client/components/ImagePlaceholder.js
+++ src/client/components/ImagePlaceholder.js
@@ -1,33 +1,32 @@
 import React from 'react';
 
 // eslint-disable-next-line arrow-parens
-const ImageLoader = props => {
-  let placeholders = [];
+const ImagePlaceholder = props => {
+  const placeholders = [];
 
   for (let i = 0; i < props.amount; i++) {
     placeholders.push(
       <div className="ui inverted card" key={`placeholder-${i}`}>
         <div className="image">
           <div className="ui inverted placeholder">
-            <div className="square image"></div>
+            <div className="square image" />
           </div>
         </div>
         <div className="content">
           <div className="ui inverted placeholder">
             <div className="header">
-              <div className="very short line"></div>
-              <div className="medium line"></div>
+              <div className="very short line" />
+              <div className="medium line" />
             </div>
             <div className="paragraph">
-              <div className="short line"></div>
+              <div className="short line" />
             </div>
           </div>
         </div>
       </div>
     );
   }
-
   return placeholders;
 };
 
-export default ImageLoader;
+export default ImagePlaceholder;
