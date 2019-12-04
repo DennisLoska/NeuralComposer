722d54a522d655bd04ad7fee136a79820f1fa3b5
Dennis <d.loska@hotmail.de>
2019-12-04 04:54:34 +0100

    add: Empty files for future components

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axe from 'react-axe';
+
 import App from './components/App';
 import * as serviceWorker from '../../serviceWorker';
 
