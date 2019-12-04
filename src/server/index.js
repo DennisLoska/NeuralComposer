a4c2520bc838f490d6b0ae4472eb8baf736e2057
Dennis <d.loska@hotmail.de>
2019-12-04 04:49:19 +0100

    feat: Adding data API and server

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,13 @@
 /* eslint-disable function-paren-newline */
 /* eslint-disable implicit-arrow-linebreak */
 const express = require('express');
-const os = require('os');
+const artStyles = require('./art_styles.json');
 
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static('public'));
 
-app.get('/api/getUsername', (req, res) =>
-  res.send({ username: os.userInfo().username })
-);
+app.get('/api/getArtStyles', (req, res) => res.send(artStyles));
 
 app.listen(process.env.PORT || 8080, () =>
   console.log(`Listening on port ${process.env.PORT || 8080}!`)
